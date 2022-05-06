e5a7350a1df22b22d12f0c855cfc94718e5d75d8
tgalvao227 <54628152+tgalvao227@users.noreply.github.com>
2022-05-06 19:48:42 -0300

    Add files via upload

--- models.py
+++ models.py
@@ -11,8 +11,14 @@ class Post(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String, nullable=False)
+    cat = db.Column(db.String, nullable=False)
+    price = db.Column(db.String, nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False)
 
-    def __init__(self, text):
+    def __init__(self, text, cat, price):
         self.text = text
+        self.cat = cat
+        self.price = price
         self.date_posted = datetime.datetime.now()
+    
+
